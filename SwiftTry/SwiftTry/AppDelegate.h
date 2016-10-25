a0abe86c92bd6f6f343694a861dd8d89c9e645f1
Andrew <shevtsov.ap@gmail.com>
2016-10-25 11:30:54 +0300

    initial commit

---
+++ SwiftTry/SwiftTry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwiftTry
+//
+//  Created by ASH on 10/25/16.
+//  Copyright © 2016 SAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
